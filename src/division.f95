f0b25e74ffb26b061eff6f50d1405f68f11a7a2b
Joseph-Chierichella <josephchierichella53@gmail.com>
2025-07-08 11:36:53 -0400

    Update division.f95

--- src/division.f95
+++ src/division.f95
@@ -3,7 +3,7 @@ SUBROUTINE division(div1,div2,div3) BIND(C , NAME = "division")
   IMPLICIT NONE
   REAL (C_DOUBLE) , VALUE , INTENT (IN) :: div1 , div2
   REAL (C_DOUBLE) , INTENT(OUT) :: div3
-  IF (div1 == 0 .OR. div2 == 0) THEN
+  IF (div2 == 0) THEN
     PRINT *, "Can't divide by 0"
   ELSE
     div3 = div1 / div2
